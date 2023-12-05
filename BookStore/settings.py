9c925a6891b284a895a7e7d0c7d6a7f9f1fb61c4
Arfaoui-ghaith <ghaith.arfaoui34@gmail.com>
2023-12-05 02:34:34 +0100

    push

--- BookStore/settings.py
+++ BookStore/settings.py
@@ -9,9 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-import os
-import django_heroku
-import dj_database_url
+
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,7 +77,7 @@ WSGI_APPLICATION = 'BookStore.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-"""DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'bookstore',
@@ -87,7 +85,7 @@ WSGI_APPLICATION = 'BookStore.wsgi.application'
         'USER': 'Arfaoui-ghaith',
         'PASSWORD': 'xdyjHobBQ1z7'
     }
-}"""
+}
 
 
 # Password validation
@@ -160,6 +158,4 @@ LOGOUT_REDIRECT_URL = 'index'
 
 
 INSTALLED_APPS += ['django_social_share']
-DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
-django_heroku.settings(locals())
 
